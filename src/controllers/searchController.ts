e8b39e7576939517362db8d05699d6edef10446e
klausmullerDev <klaussmmuller@gmail.com>
2025-03-24 17:48:44 -0300

    Projeto Finalizado

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pets";
+import { createMenuObject } from '../helpers/createMenuObject'
+
 
 export const search = (req: Request, res: Response) => {
-    //res.render("pages/search");
+    let query: string = req.query.q as string;
+
+    if(!query){
+        return res.redirect("/");
+        
+    }
+
+    let list = Pet.getFromName(query);
+    
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+        
+
+    })
 }
--- src/server.ts
+++ src/server.ts
@@ -21,8 +21,9 @@ server.use(express.static(path.join(__dirname, '../public')))
 server.use(mainRoutes);
 
 server.use((req, res) => {
-    res.send("Page not found");
+    res.render("pages/404");
 });
+
 
 
 
